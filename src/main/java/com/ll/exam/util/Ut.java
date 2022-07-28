be17599196109144d6fef5b672662d44fd7724cc
castlehyeon <qnftktls118@naver.com>
2022-07-28 11:40:55 +0900

    json을 다시 객체로, 지네릭 도입

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -5,11 +5,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Ut {
     public static class json {
-        public static String toJsonStr(Object obj, String defaultValue) {
-            ObjectMapper om = new ObjectMapper();
+        //ut클래스 내부에 static으로 클래스를 만듦.
+//        public static String toStr(Object obj, String defaultValue) {
+//            ObjectMapper om = new ObjectMapper();
 
+            private static final ObjectMapper om;
+
+            static {
+                om = new ObjectMapper();
+            }
+        public static String toStr(Object obj, String defaultValue) {
             try {
                 return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+        //json을 다시 객체화
+        public static <T> T toObj(String jsonStr, Class<T> cls, T defaultValue) {
+            try {
+                return (T) om.readValue(jsonStr, cls);
             } catch (JsonProcessingException e) {
                 return defaultValue;
             }
